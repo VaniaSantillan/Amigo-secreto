a8b3f1bb189b8dd915b1461af670787f38d6f026
Vania Santillan Aguilar <142811154+VaniaSantillan@users.noreply.github.com>
2025-01-25 22:45:46 -0600

    Validación de los nombres

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 // Array para almacenamiento de los nombres.
 let amigos = [];
 
-// Funcioón para agregar un amigo a la lista 
+// Funcioón para agregar un amigo a la lista.
 function agregarAmigo(){
-    
+
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    //Para validar si el campo esta vacío.
+    if(nombreAmigo === 0){
+        alert('Por favor, ingresa un nombre válido');
+        return;
+    }
+
 }
