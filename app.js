5bb3f7cb3296ee20bd611500add57f94a3a240e5
Vania Santillan Aguilar <142811154+VaniaSantillan@users.noreply.github.com>
2025-01-25 22:55:18 -0600

    Actualización de lista

--- app.js
+++ app.js
@@ -9,8 +9,31 @@ function agregarAmigo(){
 
     //Para validar si el campo esta vacío.
     if(nombreAmigo === 0){
-        alert('Por favor, ingresa un nombre válido');
+        alert("Por favor, ingresa un nombre válido");
         return;
     }
 
+    //Agrega el nombre a la lista de amigos.
+    amigos.push(nombreAmigo);
+
+    //Limpiar el campo de entrada.
+    inputAmigo.value = '';
+
+    //Actualizar la lista de amigos en la interfaz.
+    actualizarLista();
+
+}
+
+//Función para actualizar la lista de amigos.
+function actualizarLista(){
+
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = '';
+
+    //Añadir a los amigos a la lista.
+    for (let i = 0; i < amigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
 }
